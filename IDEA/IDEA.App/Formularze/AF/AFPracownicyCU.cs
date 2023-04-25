e024a52bf0e8d80414de1fa4bcc350c645b102cb
Sylwia <sylwia.jankowska@student.wat.edu.pl>
2023-04-25 12:03:32 +0200

    Formularz Pracownicy

---
+++ IDEA/IDEA.App/Formularze/AF/AFPracownicyCU.cs
@@ -0,0 +1,110 @@
+﻿using IDEA.Database;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace IDEA.App
+{
+    public partial class AFPracownicyCU : Form
+    {
+        private bool flagEdit = false;
+        Pracownicy selectedPracownicy = new Pracownicy();
+        IDEAEntities db = IDEADatabase.GetInstance();
+
+        //Wersja Dodawanie
+        public AFPracownicyCU()
+        {
+            InitializeComponent();
+        }
+        //Wersja Edycja
+        public AFPracownicyCU(Pracownicy _selectedPracownicy)
+        {
+            flagEdit = true;
+            InitializeComponent();
+            selectedPracownicy = _selectedPracownicy;
+            lblKindWindow.Text = "Edytowanie Istniejącego Pracownika";
+            txtImie.Text = selectedPracownicy.Imie;
+            txtNazwisko.Text = selectedPracownicy.Nazwisko;
+            txtPESEL.Text = selectedPracownicy.PESEL;
+            txtUlica.Text = selectedPracownicy.Adres_Ulica;
+            maskTxtKod.Text = selectedPracownicy.Adres_Kod_Pocztowy;
+            txtMiasto.Text = selectedPracownicy.Adres_Miasto;
+            txtTelefon.Text = selectedPracownicy.Telefon;
+            txtEmail.Text = selectedPracownicy.E_mail;
+            txtNumerKontaBankowego.Text = selectedPracownicy.Numer_Konta_Bankowego;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            if (flagEdit)
+            {
+                //Edycja
+                Pracownicy updatePracownicy = db.Pracownicies.First(p => p.ID_Pracownicy == selectedPracownicy.ID_Pracownicy);
+                updatePracownicy.Imie = txtImie.Text;
+                updatePracownicy.Nazwisko = txtNazwisko.Text;
+                updatePracownicy.PESEL = txtPESEL.Text;
+                updatePracownicy.Adres_Ulica = txtUlica.Text;
+                updatePracownicy.Adres_Kod_Pocztowy = maskTxtKod.Text;
+                updatePracownicy.Adres_Miasto = txtMiasto.Text;
+                updatePracownicy.Telefon = txtTelefon.Text;
+                updatePracownicy.E_mail = txtEmail.Text;
+                updatePracownicy.Numer_Konta_Bankowego = txtNumerKontaBankowego.Text;
+                db.SaveChanges();
+            }
+            else
+            {
+                //Dodanie nowego pracownika
+                Pracownicy PracownikNew = new Pracownicy();
+                PracownikNew.Imie = txtImie.Text;
+                PracownikNew.Nazwisko = txtNazwisko.Text;
+                PracownikNew.PESEL = txtPESEL.Text;
+                PracownikNew.Adres_Ulica = txtUlica.Text;
+                PracownikNew.Adres_Kod_Pocztowy = maskTxtKod.Text;
+                PracownikNew.Adres_Miasto = txtMiasto.Text;
+                PracownikNew.Telefon = txtTelefon.Text;
+                PracownikNew.E_mail = txtEmail.Text;
+                PracownikNew.Numer_Konta_Bankowego = txtNumerKontaBankowego.Text;
+                db.Pracownicies.Add(PracownikNew);
+                db.SaveChanges();
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void AFKlienciCU_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtImie_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblKindWindow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AFPracownicyCU_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- IDEA/IDEA.App/Formularze/AF/AFPracownicyForm.cs
+++ IDEA/IDEA.App/Formularze/AF/AFPracownicyForm.cs
@@ -11,7 +11,7 @@ namespace IDEA.App
         IDEAEntities db = IDEADatabase.GetInstance();
         private bool flagSelected = false;
         //private IDEAEntities db;
-        Klient selectedKlient = new Klient();
+        Pracownicy selectedPracownicy = new Pracownicy();
 
         public AFPracownicyForm()
         {
@@ -22,56 +22,83 @@ namespace IDEA.App
             toolTipModify.SetToolTip(iBtnEdit, "Edytuj");
             ToolTip toolTipDelete = new ToolTip();
             toolTipDelete.SetToolTip(iBtnDelete, "Usuń");
-            initDgwKlienci();
+            initDgwPracownicy();
         }
 
-        private void initDgwKlienci()
+        private void initDgwPracownicy()
         {
-            dgvKlienci.DataSource = db.Klients.ToList();
-            this.dgvKlienci.Columns["ID_Klient"].Visible = false;
-            dgvKlienci.Columns["Kontrola_Jakosci_Zamowienia"].Visible = false;
-            dgvKlienci.Columns["Sklad_Zamowienia"].Visible = false;
-            dgvKlienci.Columns["Zamowienia_Klienci"].Visible = false;
-            dgvKlienci.Columns["Imie"].HeaderText = "Imię";
-            dgvKlienci.Columns["Nazwa_Podmiotu"].HeaderText = "Nazwa Podmiotu";
-            dgvKlienci.Columns["Adres_Ulica"].HeaderText = "Ulica";
-            dgvKlienci.Columns["Adres_Kod_Pocztowy"].HeaderText = "Kod pocztowy";
-            dgvKlienci.Columns["Adres_Miasto"].HeaderText = "Miasto";
-            dgvKlienci.Columns["E_mail"].HeaderText = "Email";
-            dgvKlienci.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            dgvPracownicy.DataSource = db.Pracownicies.ToList();
+            this.dgvPracownicy.Columns["ID_Pracownicy"].Visible = false;
+            dgvPracownicy.Columns["Badanie_Maszyny"].Visible = false;
+            dgvPracownicy.Columns["Zamowienia_Klienci"].Visible = false;
+            dgvPracownicy.Columns["Pracownicy_Zatrudnienie"].Visible = false;
+            dgvPracownicy.Columns["Pracownicy_RodzajUmowy"].Visible = false;
+            dgvPracownicy.Columns["Pracownicy_Dzialy"].Visible = false;
+            dgvPracownicy.Columns["Pracownicy_Stanowisko"].Visible = false;
+            dgvPracownicy.Columns["Pracownicy_Jezyki"].Visible = false;
+            dgvPracownicy.Columns["Koszty_Rozne"].Visible = false;
+            dgvPracownicy.Columns["Dostawas"].Visible = false;
+            dgvPracownicy.Columns["Fakturies"].Visible = false;
+            dgvPracownicy.Columns["Obslugis"].Visible = false;
+            dgvPracownicy.Columns["ObslugiPojazdows"].Visible = false;
+            dgvPracownicy.Columns["Proces_Pracownicy"].Visible = false;
+            dgvPracownicy.Columns["RozlozeniePolki_Materialy"].Visible = false;
+            dgvPracownicy.Columns["RozlozeniePolki_Produkty"].Visible = false;
+            dgvPracownicy.Columns["SkladWysylka_Produkt"].Visible = false;
+            dgvPracownicy.Columns["StanLicznikas"].Visible = false;
+            dgvPracownicy.Columns["TransportWewnetrznies"].Visible = false;
+            dgvPracownicy.Columns["Urlopies"].Visible = false;
+            dgvPracownicy.Columns["Wysylkas"].Visible = false;
+            dgvPracownicy.Columns["Zlecenie_Magazynowe"].Visible = false;
+            dgvPracownicy.Columns["Oplaty_Administracyjne"].Visible = false;
+            dgvPracownicy.Columns["Dokumentacja_Pracownicy"].Visible = false;
+            dgvPracownicy.Columns["Imie"].HeaderText = "Imię";
+            dgvPracownicy.Columns["Nazwisko"].HeaderText = "Nazwisko";
+            dgvPracownicy.Columns["PESEL"].HeaderText = "PESEL";
+            dgvPracownicy.Columns["Adres_Ulica"].HeaderText = "Ulica";
+            dgvPracownicy.Columns["Adres_Kod_Pocztowy"].HeaderText = "Kod pocztowy";
+            dgvPracownicy.Columns["Adres_Miasto"].HeaderText = "Miasto";
+            dgvPracownicy.Columns["E_mail"].HeaderText = "Email";
+            dgvPracownicy.Columns["Telefon"].HeaderText = "Telefon";
+            dgvPracownicy.Columns["Numer_Konta_Bankowego"].HeaderText = "Numer konta bankowego";
+
+            dgvPracownicy.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
         }
-        private void AFKlienciForm_Load(object sender, EventArgs e)
+        private void AFPracownicyForm_Load(object sender, EventArgs e)
         {
-            dgvKlienci.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            dgvPracownicy.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
         }
-        private void dgvKlienci_CellClick(object sender, DataGridViewCellEventArgs e)
+        private void dgvPracownicy_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             flagSelected = true;
             int index;
-            index = dgvKlienci.CurrentRow.Index;
+            index = dgvPracownicy.CurrentRow.Index;
 
-            DataGridViewRow selectedrow = dgvKlienci.Rows[index];
-            selectedKlient.ID_Klient = int.Parse(selectedrow.Cells[0].Value.ToString());
-            selectedKlient.Imie = selectedrow.Cells[1].Value.ToString();
-            selectedKlient.Nazwisko = selectedrow.Cells[2].Value.ToString();
-            selectedKlient.Nazwa_Podmiotu = selectedrow.Cells[3].Value.ToString();
-            selectedKlient.NIP = selectedrow.Cells[4].Value.ToString();
-            selectedKlient.Adres_Ulica = selectedrow.Cells[5].Value.ToString();
-            selectedKlient.Adres_Kod_Pocztowy = selectedrow.Cells[6].Value.ToString();
-            selectedKlient.Adres_Miasto = selectedrow.Cells[7].Value.ToString();
-            selectedKlient.Telefon = selectedrow.Cells[8].Value.ToString();
-            selectedKlient.E_mail = selectedrow.Cells[9].Value.ToString();
+            DataGridViewRow selectedrow = dgvPracownicy.Rows[index];
+            selectedPracownicy.ID_Pracownicy = int.Parse(selectedrow.Cells[0].Value.ToString());
+            selectedPracownicy.Imie = selectedrow.Cells[1].Value.ToString();
+            selectedPracownicy.Nazwisko = selectedrow.Cells[2].Value.ToString();
+            selectedPracownicy.PESEL = selectedrow.Cells[3].Value.ToString();
+            selectedPracownicy.Adres_Ulica = selectedrow.Cells[4].Value.ToString();
+            selectedPracownicy.Adres_Kod_Pocztowy = selectedrow.Cells[5].Value.ToString();
+            selectedPracownicy.Adres_Miasto = selectedrow.Cells[6].Value.ToString();
+            selectedPracownicy.Numer_Konta_Bankowego = selectedrow.Cells[7].Value.ToString();
+            selectedPracownicy.Telefon = selectedrow.Cells[8].Value.ToString();
+            selectedPracownicy.E_mail = selectedrow.Cells[9].Value.ToString();
+
+
+
         }
 
 
         //Wersja Dodawanie
         private void iBtnNew_Click(object sender, EventArgs e)
         {
-            //openKlientEdition(sender);
-            using (AFKlienciCU aF = new AFKlienciCU())
+            //openPracownikEdition(sender);
+            using (AFPracownicyCU aF = new AFPracownicyCU())
             {
                 aF.ShowDialog();
-                initDgwKlienci();
+                initDgwPracownicy();
             }
         }
         //Wersja Edycja
@@ -79,31 +106,31 @@ namespace IDEA.App
         {
             if (flagSelected)
             {
-                using (AFKlienciCU aF = new AFKlienciCU(selectedKlient))
+                using (AFPracownicyCU aF = new AFPracownicyCU(selectedPracownicy))
                 {
                     aF.ShowDialog();
-                    initDgwKlienci();
+                    initDgwPracownicy();
                 }
             }
             else
             {
-                MessageBox.Show("Nie wybrano klienta do edycji!");
+                MessageBox.Show("Nie wybrano pracownika do edycji!");
             }
 
         }
 
         private void iBtnDelete_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = MessageBox.Show("Czy chcesz usunąć?\n" + selectedKlient.Imie + " " + selectedKlient.Nazwisko, "Usuwanie", MessageBoxButtons.YesNo);
+            DialogResult dialogResult = MessageBox.Show("Czy chcesz usunąć?\n" + selectedPracownicy.Imie + " " + selectedPracownicy.Nazwisko, "Usuwanie", MessageBoxButtons.YesNo);
             if (dialogResult == DialogResult.Yes)
             {
-                var query = from p in db.Klients
-                            where p.ID_Klient == selectedKlient.ID_Klient
+                var query = from p in db.Pracownicies
+                            where p.ID_Pracownicy == selectedPracownicy.ID_Pracownicy
                             select p;
-                foreach (Klient p in query)
-                    db.Klients.Remove(p);
+                foreach (Pracownicy p in query)
+                    db.Pracownicies.Remove(p);
                 db.SaveChanges();
-                initDgwKlienci();
+                initDgwPracownicy();
             }
             else if (dialogResult == DialogResult.No)
             {
@@ -117,19 +144,48 @@ namespace IDEA.App
         {
             string filtr = txtSearch.Text;
 
-            dgvKlienci.DataSource = db.Klients.Where(k =>
+            dgvPracownicy.DataSource = db.Pracownicies.Where(k =>
                k.Imie.Contains(filtr)
             || k.Nazwisko.Contains(filtr)
-            || k.Nazwa_Podmiotu.Contains(filtr)
-            || k.NIP.Contains(filtr)
+            || k.PESEL.Contains(filtr)
             || k.Adres_Ulica.Contains(filtr)
             || k.Adres_Kod_Pocztowy.Contains(filtr)
             || k.Adres_Miasto.Contains(filtr)
             || k.Telefon.Contains(filtr)
-            || k.E_mail.Contains(filtr)).ToList();
+            || k.E_mail.Contains(filtr)
+            || k.Numer_Konta_Bankowego.Contains(filtr)).ToList();
+            dgvPracownicy.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
 
-            dgvKlienci.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
 
+        }
+
+        private void btnDzial_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStanowisko_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRodzajUmowy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnZatrudnienie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnJezyki_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvPracownicy_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
         }
     }
