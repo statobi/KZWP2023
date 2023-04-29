0b49d8eee93ab298efbbe0acf3e5df67b06d8b45
JohnStrong98 <jan.majnert@student.wat.edu.pl>
2023-04-29 23:53:35 +0200

    Psucie weekendu przez KWZP 2

--- IDEA/IDEA.App/Formularze/Produkcja/EksploatacjaMaszynForm.Designer.cs
+++ IDEA/IDEA.App/Formularze/Produkcja/EksploatacjaMaszynForm.Designer.cs
@@ -40,13 +40,17 @@
             this.label7 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.cbRodzajStrategiiEksploatacji = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvEksploatacjaMaszyn = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEksploatacjaMaszyn)).BeginInit();
             this.SuspendLayout();
             // 
             // cbParametrMaszyny
             // 
             this.cbParametrMaszyny.FormattingEnabled = true;
-            this.cbParametrMaszyny.Location = new System.Drawing.Point(364, 137);
+            this.cbParametrMaszyny.Location = new System.Drawing.Point(339, 298);
             this.cbParametrMaszyny.Margin = new System.Windows.Forms.Padding(2);
             this.cbParametrMaszyny.Name = "cbParametrMaszyny";
             this.cbParametrMaszyny.Size = new System.Drawing.Size(373, 21);
@@ -55,7 +59,7 @@
             // txtNumerNormy
             // 
             this.txtNumerNormy.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.txtNumerNormy.Location = new System.Drawing.Point(364, 288);
+            this.txtNumerNormy.Location = new System.Drawing.Point(339, 449);
             this.txtNumerNormy.Name = "txtNumerNormy";
             this.txtNumerNormy.Size = new System.Drawing.Size(187, 29);
             this.txtNumerNormy.TabIndex = 73;
@@ -64,7 +68,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label13.Location = new System.Drawing.Point(64, 288);
+            this.label13.Location = new System.Drawing.Point(39, 449);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(126, 24);
             this.label13.TabIndex = 72;
@@ -73,7 +77,7 @@
             // txtWartoscNominalnaParametru
             // 
             this.txtWartoscNominalnaParametru.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.txtWartoscNominalnaParametru.Location = new System.Drawing.Point(364, 170);
+            this.txtWartoscNominalnaParametru.Location = new System.Drawing.Point(339, 331);
             this.txtWartoscNominalnaParametru.Name = "txtWartoscNominalnaParametru";
             this.txtWartoscNominalnaParametru.Size = new System.Drawing.Size(187, 29);
             this.txtWartoscNominalnaParametru.TabIndex = 71;
@@ -82,7 +86,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label10.Location = new System.Drawing.Point(66, 168);
+            this.label10.Location = new System.Drawing.Point(41, 329);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(259, 24);
             this.label10.TabIndex = 70;
@@ -91,7 +95,7 @@
             // txtWartoscMaksymalnaParametru
             // 
             this.txtWartoscMaksymalnaParametru.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.txtWartoscMaksymalnaParametru.Location = new System.Drawing.Point(364, 248);
+            this.txtWartoscMaksymalnaParametru.Location = new System.Drawing.Point(339, 409);
             this.txtWartoscMaksymalnaParametru.Name = "txtWartoscMaksymalnaParametru";
             this.txtWartoscMaksymalnaParametru.Size = new System.Drawing.Size(187, 29);
             this.txtWartoscMaksymalnaParametru.TabIndex = 69;
@@ -100,7 +104,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label9.Location = new System.Drawing.Point(64, 248);
+            this.label9.Location = new System.Drawing.Point(39, 409);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(275, 24);
             this.label9.TabIndex = 68;
@@ -109,7 +113,7 @@
             // txtWartoscMinimalnaParametru
             // 
             this.txtWartoscMinimalnaParametru.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.txtWartoscMinimalnaParametru.Location = new System.Drawing.Point(364, 205);
+            this.txtWartoscMinimalnaParametru.Location = new System.Drawing.Point(339, 366);
             this.txtWartoscMinimalnaParametru.Name = "txtWartoscMinimalnaParametru";
             this.txtWartoscMinimalnaParametru.Size = new System.Drawing.Size(187, 29);
             this.txtWartoscMinimalnaParametru.TabIndex = 67;
@@ -118,7 +122,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label8.Location = new System.Drawing.Point(64, 205);
+            this.label8.Location = new System.Drawing.Point(39, 366);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(257, 24);
             this.label8.TabIndex = 66;
@@ -128,7 +132,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.Location = new System.Drawing.Point(64, 133);
+            this.label7.Location = new System.Drawing.Point(39, 294);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(163, 24);
             this.label7.TabIndex = 65;
@@ -140,7 +144,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 53);
+            this.panel1.Size = new System.Drawing.Size(896, 53);
             this.panel1.TabIndex = 75;
             // 
             // label1
@@ -148,18 +152,49 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(297, 9);
+            this.label1.Location = new System.Drawing.Point(345, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(215, 25);
             this.label1.TabIndex = 55;
             this.label1.Text = "Eksploatacja maszyn";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
+            // cbRodzajStrategiiEksploatacji
+            // 
+            this.cbRodzajStrategiiEksploatacji.FormattingEnabled = true;
+            this.cbRodzajStrategiiEksploatacji.Location = new System.Drawing.Point(323, 53);
+            this.cbRodzajStrategiiEksploatacji.Margin = new System.Windows.Forms.Padding(2);
+            this.cbRodzajStrategiiEksploatacji.Name = "cbRodzajStrategiiEksploatacji";
+            this.cbRodzajStrategiiEksploatacji.Size = new System.Drawing.Size(373, 21);
+            this.cbRodzajStrategiiEksploatacji.TabIndex = 77;
+            this.cbRodzajStrategiiEksploatacji.SelectedIndexChanged += new System.EventHandler(this.cbRodzajStrategiiEksploatacji_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(23, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(238, 24);
+            this.label6.TabIndex = 76;
+            this.label6.Text = "Rodzaj strategii eksploatacji";
+            // 
+            // dgvEksploatacjaMaszyn
+            // 
+            this.dgvEksploatacjaMaszyn.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEksploatacjaMaszyn.Location = new System.Drawing.Point(27, 101);
+            this.dgvEksploatacjaMaszyn.Name = "dgvEksploatacjaMaszyn";
+            this.dgvEksploatacjaMaszyn.Size = new System.Drawing.Size(840, 150);
+            this.dgvEksploatacjaMaszyn.TabIndex = 78;
+            // 
             // EksploatacjaMaszynForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(896, 610);
+            this.Controls.Add(this.dgvEksploatacjaMaszyn);
+            this.Controls.Add(this.cbRodzajStrategiiEksploatacji);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.cbParametrMaszyny);
             this.Controls.Add(this.txtNumerNormy);
@@ -175,6 +210,7 @@
             this.Text = "EksploatacjaMaszynForm";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEksploatacjaMaszyn)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,5 +230,8 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbRodzajStrategiiEksploatacji;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvEksploatacjaMaszyn;
     }
 }
--- IDEA/IDEA.App/Formularze/Produkcja/EksploatacjaMaszynForm.cs
+++ IDEA/IDEA.App/Formularze/Produkcja/EksploatacjaMaszynForm.cs
@@ -1,12 +1,7 @@
 ﻿using IDEA.Database;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IDEA.App.Formularze.Produkcja
@@ -19,7 +14,27 @@ namespace IDEA.App.Formularze.Produkcja
         {
             InitializeComponent();
             initOpcjeParametrMaszyny();
+            initOpcjeRodzajStrategiiEksploatacji();
+            //initDgvEksploatacja_PP();
+            // initDgvEksploatacja_ST();
         }
+
+        private void initOpcjeRodzajStrategiiEksploatacji()
+
+        {
+            var RodzajeStrategiiEksploatacji = db.Rodzaj_Strategii_Eksp
+                    .Select(s => new { s.ID_Rodzaj_Strategii_Eksp, s.Nazwa }).ToList();
+            cbRodzajStrategiiEksploatacji.DataSource = RodzajeStrategiiEksploatacji;
+            cbRodzajStrategiiEksploatacji.ValueMember = "ID_Rodzaj_Strategii_Eksp";
+            cbRodzajStrategiiEksploatacji.DisplayMember = "Nazwa";
+            cbRodzajStrategiiEksploatacji.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbRodzajStrategiiEksploatacji.SelectedIndex = -1;
+        }
+
+
+
+
+
 
         private void initOpcjeParametrMaszyny()
         {
@@ -30,5 +45,24 @@ namespace IDEA.App.Formularze.Produkcja
             cbParametrMaszyny.SelectedIndex = -1;
         }
 
+        private void cbRodzajStrategiiEksploatacji_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbRodzajStrategiiEksploatacji.Text == "Strategia eksploatacji według planowanej profilaktyki")
+            {
+                dgvEksploatacjaMaszyn.DataSource = db.Widok_Model_Stategia_PP.ToList();
+                dgvEksploatacjaMaszyn.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+            }
+            else if (cbRodzajStrategiiEksploatacji.Text == "Strategia eksploatacji według stanu technicznego")
+            {
+                dgvEksploatacjaMaszyn.DataSource = db.Widok_Model_Strategia_ST.ToList();
+                dgvEksploatacjaMaszyn.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+            }
+            else
+            {
+                //MessageBox.Show("test");
+            }
+
+        }
     }
 }
