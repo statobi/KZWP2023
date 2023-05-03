8a263661fbd767440183423061b25b0ec99e51b9
Mateusz Szymański <mateuszwojciechszymanski@gmail.com>
2023-05-03 20:45:38 +0200

    dodawanie zlecenia magazynowego dla zakonczonych zlecen

--- IDEA/IDEA.App/Formularze/Produkcja/ProdukcjaKonczenieProdukcjiForm.cs
+++ IDEA/IDEA.App/Formularze/Produkcja/ProdukcjaKonczenieProdukcjiForm.cs
@@ -22,6 +22,7 @@ namespace IDEA.App.Formularze.Produkcja
             InitializeComponent();
             InitZakonczeniaProukcji();
             initDatePickers();
+            initWyborPracownicy();
         }
 
 
@@ -56,6 +57,7 @@ namespace IDEA.App.Formularze.Produkcja
                 StatusPakowania.Data = dateWysylki.Value;
                 db.ZamowieniaKlienci_StatusZamowienia.Add(StatusPakowania);
                 db.SaveChanges();
+                Dodawaniezleceniamagazynowego();
                 MessageBox.Show("Przekazano do Pakowania");
             }
             else if (Zapytaniewyslania == DialogResult.No)
@@ -63,7 +65,54 @@ namespace IDEA.App.Formularze.Produkcja
 
             }
         }
+        private void Dodawaniezleceniamagazynowego()
+        {
+            var listaCalegoSkladuZamowienia = db.Sklad_Zamowienia
+                .Where(x =>x.ID_Zamowienia_Klienci == ZamowieniedoPakowania.ID_Zamowienia_Klienci)
+                .Select(x =>x.ID_Sklad_Zamowienia) .ToList();
 
+            string WybranyPracownik = cbPracownik.Text;
+            var IDPracwonika = db.Pracownicies
+            .Where(x => x.Nazwisko == WybranyPracownik)
+            .Select(x => x.ID_Pracownicy)
+            .FirstOrDefault();
+
+
+            for (int i = 0; i < listaCalegoSkladuZamowienia.Count; i++)
+            {
+                Zlecenie_Magazynowe Zlecenie = new Zlecenie_Magazynowe();
+                Zlecenie.ID_Sklad_Zamowienia = listaCalegoSkladuZamowienia[i];
+                Zlecenie.ID_Pracownicy = IDPracwonika;
+                Zlecenie.Data = dateWysylki.Value;
+                Zlecenie.CzyZlecenieStale = "Nie";
+                db.Zlecenie_Magazynowe.Add(Zlecenie);
+                db.SaveChanges();
+                var zlecenieprodukt = db.Zlecenie_Magazynowe
+                    .Where(x => x.ID_Sklad_Zamowienia== Zlecenie.ID_Sklad_Zamowienia && x.CzyZlecenieStale == Zlecenie.CzyZlecenieStale && x.ID_Pracownicy == Zlecenie.ID_Pracownicy && x.Data == Zlecenie.Data)
+                    .Select(x => x.ID_Zlecenie_Magazynowe)
+                    .FirstOrDefault();
+                var dodanieproduktu =  db.Sklad_Zamowienia
+                .Where(x => x.ID_Sklad_Zamowienia == listaCalegoSkladuZamowienia[i])
+                .Select(x => x.ID_Produkt).ToList();
+                for (int z = 0; z < dodanieproduktu.Count; z++)
+                {
+                    Sklad_Zlecenie_Produkt NoweZlecenieProdukt = new Sklad_Zlecenie_Produkt();
+                    NoweZlecenieProdukt.ID_Zlecenie_Magazynowe = zlecenieprodukt;
+                    NoweZlecenieProdukt.ID_Produkt = dodanieproduktu[z];
+                    var iloscproduktow = db.Sklad_Zamowienia
+                        .Where(x => x.ID_Sklad_Zamowienia == Zlecenie.ID_Sklad_Zamowienia && x.ID_Produkt == dodanieproduktu[z])
+                        .Select(x =>x.Ilosc)
+                        .FirstOrDefault();
+                    NoweZlecenieProdukt.IloscProduktow= iloscproduktow;
+                    db.Sklad_Zlecenie_Produkt.Add(NoweZlecenieProdukt);
+                    db.SaveChanges();
+
+                }
+
+
+            }
+
+        }
 
         private void dgvZakonczenie_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -74,6 +123,22 @@ namespace IDEA.App.Formularze.Produkcja
         private void btnPakowanie_Click(object sender, EventArgs e)
         {
             Pakowanie();
+        }
+        private void initWyborPracownicy()
+        {
+            //cbPracownik.DataSource = null;
+            //cbPracownik.Text= string.Empty;
+            //cbPracownik.Items.Clear();
+            var WyborPracownika = db.V_Operatorzy_Maszyn
+                    .Select(s => s.Nazwisko).ToList();
+            cbPracownik.DataSource = WyborPracownika;
+            //cbPracownik.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbPracownik.SelectedIndex = -1;
+
+        }
+        private void ProdukcjaKonczenieProdukcjiForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
