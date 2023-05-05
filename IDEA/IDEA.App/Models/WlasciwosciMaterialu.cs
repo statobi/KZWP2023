00a5d8146063542040c4a9d40272208e657fa7d5
Mateusz Szymański <mateuszwojciechszymanski@gmail.com>
2023-05-05 22:15:13 +0200

    Aktualizacja materialow

---
+++ IDEA/IDEA.App/Models/WlasciwosciMaterialu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDEA.App.Models
+{
+    internal class WlasciwosciMaterialu
+    {
+        public int ID_Material { get; set; }
+
+        public string Nazwa_Wlasciwosci { get; set; }
+
+        public string Wartosc { get; set; }
+
+        public string Jednostka { get; set; }
+    }
+}
