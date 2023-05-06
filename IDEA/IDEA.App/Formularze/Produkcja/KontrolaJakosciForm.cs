2c540d477c98e2c66a6b6d16901993d5a59d37d0
Mateusz Szymański <mateuszwojciechszymanski@gmail.com>
2023-05-06 13:01:07 +0200

    poprawa edytowania kontroli jakosci

--- IDEA/IDEA.App/Formularze/Produkcja/KontrolaJakosciForm.cs
+++ IDEA/IDEA.App/Formularze/Produkcja/KontrolaJakosciForm.cs
@@ -13,7 +13,7 @@ namespace IDEA.App
         IDEAEntities db = IDEADatabase.GetInstance();
         private bool flagSelected = false;
         //private IDEAEntities db;
-        V_Kontrola_Jakosci selectedV_Kontrola_Jakosci = new V_Kontrola_Jakosci();
+        Kontrola_Jakosci_Zamowienia selectedV_Kontrola_Jakosci = new Kontrola_Jakosci_Zamowienia();
 
         public KontrolaJakosciForm()
         {
@@ -26,23 +26,23 @@ namespace IDEA.App
             toolTipDelete.SetToolTip(iBtnDelete, "Usuń");
             initDgwKlienci();
             initDGVZamowienia();
-            dgvZamowienia.SelectionChanged += dgvZamowienia_SelectionChanged;
+            //dgvZamowienia.SelectionChanged += dgvZamowienia_SelectionChanged;
         }
-        private void refreshDgwKlienci()
-        {
-            // Usunięcie źródła danych
-            dgvKlienci.DataSource = null;
+        //private void refreshDgwKlienci()
+        //{
+        //    // Usunięcie źródła danych
+        //    dgvKlienci.DataSource = null;
 
-            // Ponowne załadowanie danych
-            List<V_Kontrola_Jakosci> listaKontroliJakosci = db.V_Kontrola_Jakosci.ToList();
+        //    // Ponowne załadowanie danych
+        //    List<V_Kontrola_Jakosci> listaKontroliJakosci = db.V_Kontrola_Jakosci.ToList();
 
-            // Przypisanie zaktualizowanej listy danych do kontrolki DataGridView
-            dgvKlienci.DataSource = listaKontroliJakosci;
+        //    // Przypisanie zaktualizowanej listy danych do kontrolki DataGridView
+        //    dgvKlienci.DataSource = listaKontroliJakosci;
 
-            // Odświeżenie kontrolki DataGridView
-            dgvKlienci.Refresh();
-            dgvKlienci.Invalidate();
-        }
+        //    // Odświeżenie kontrolki DataGridView
+        //    dgvKlienci.Refresh();
+        //    dgvKlienci.Invalidate();
+        //}
         private void initDgwKlienci()
         {
             dgvKlienci.DataSource = db.V_Kontrola_Jakosci.ToList();
@@ -60,24 +60,24 @@ namespace IDEA.App
             dgvZamowienia.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
 
         }
-        private void dgvZamowienia_SelectionChanged(object sender, EventArgs e)
-        {
-            if (dgvZamowienia.SelectedRows.Count > 0)
-            {
-                string numerZamowienia = dgvZamowienia.SelectedRows[0].Cells["Numer_Zamowienia"].Value.ToString();
+        //private void dgvZamowienia_SelectionChanged(object sender, EventArgs e)
+        //{
+        //    if (dgvZamowienia.SelectedRows.Count > 0)
+        //    {
+        //        string numerZamowienia = dgvZamowienia.SelectedRows[0].Cells["Numer_Zamowienia"].Value.ToString();
 
-                foreach (DataGridViewRow row in dgvKlienci.Rows)
-                {
-                    string numerZamowienia1 = row.Cells["Numer_Zamowienia"].Value.ToString();
-                    if (numerZamowienia1 == numerZamowienia)
-                    {
-                        row.Selected = true;
-                        dgvKlienci.FirstDisplayedScrollingRowIndex = row.Index;
-                        break;
-                    }
-                }
-            }
-        }
+        //        foreach (DataGridViewRow row in dgvKlienci.Rows)
+        //        {
+        //            string numerZamowienia1 = row.Cells["Numer_Zamowienia"].Value.ToString();
+        //            if (numerZamowienia1 == numerZamowienia)
+        //            {
+        //                row.Selected = true;
+        //                dgvKlienci.FirstDisplayedScrollingRowIndex = row.Index;
+        //                break;
+        //            }
+        //        }
+        //    }
+        //}
         private void AFKlienciForm_Load(object sender, EventArgs e)
         {
             dgvKlienci.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
@@ -85,7 +85,22 @@ namespace IDEA.App
         private void dgvKlienci_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             flagSelected = true;
-            int index;
+            selectedV_Kontrola_Jakosci.ID_Kontrola_Jakosci_Zamowienia = int.Parse(dgvKlienci.Rows[e.RowIndex].Cells[0].Value.ToString());
+
+            var query = from p in db.Kontrola_Jakosci_Zamowienia
+                        where p.ID_Kontrola_Jakosci_Zamowienia == selectedV_Kontrola_Jakosci.ID_Kontrola_Jakosci_Zamowienia
+                        select p;
+            foreach (Kontrola_Jakosci_Zamowienia p in query)
+            {
+                selectedV_Kontrola_Jakosci.ID_Kontrola_Jakosci_Zamowienia = p.ID_Kontrola_Jakosci_Zamowienia;
+                selectedV_Kontrola_Jakosci.ID_Sklad_Zamowienia = p.ID_Sklad_Zamowienia;
+                selectedV_Kontrola_Jakosci.Zaakcpetowane = p.Zaakcpetowane;
+                selectedV_Kontrola_Jakosci.Odrzucone= p.Odrzucone;
+                selectedV_Kontrola_Jakosci.Data = p.Data;
+                selectedV_Kontrola_Jakosci.Uwagi= p.Uwagi;
+
+
+            }
 
         }
 
@@ -93,7 +108,7 @@ namespace IDEA.App
         //Wersja Dodawanie
         private void iBtnNew_Click(object sender, EventArgs e)
         {
-            refreshDgwKlienci();
+            //refreshDgwKlienci();
 
             //using (KontrolaJakosciFormCU aF = new KontrolaJakosciFormCU())
             // {
@@ -109,7 +124,7 @@ namespace IDEA.App
                 using (KontrolaJakosciFormCU aF = new KontrolaJakosciFormCU(selectedV_Kontrola_Jakosci))
                 {
                     aF.ShowDialog();
-                    refreshDgwKlienci();
+                    initDgwKlienci();
                 }
             }
             else
@@ -118,12 +133,12 @@ namespace IDEA.App
             }
             dgvKlienci.DataSource = db.V_Kontrola_Jakosci.ToList();
             dgvKlienci.Refresh();
-            //initDgwKlienci();
+            initDgwKlienci();
         }
         //usun
         private void iBtnDelete_Click(object sender, EventArgs e)
         {
-            DialogResult dialogResult = MessageBox.Show("Czy chcesz usunąć?\n" + selectedV_Kontrola_Jakosci.Data_kontroli + " " + selectedV_Kontrola_Jakosci.ID_Kontrola_Jakosci_Zamowienia, "Usuwanie", MessageBoxButtons.YesNo);
+            DialogResult dialogResult = MessageBox.Show("Czy chcesz usunąć?\n" + selectedV_Kontrola_Jakosci.ID_Kontrola_Jakosci_Zamowienia + " " + selectedV_Kontrola_Jakosci.ID_Kontrola_Jakosci_Zamowienia, "Usuwanie", MessageBoxButtons.YesNo);
             if (dialogResult == DialogResult.Yes)
             {
                 if (dgvKlienci.SelectedRows.Count > 0) // sprawdzamy czy została zaznaczona jakaś pozycja w dgvKlienci
@@ -133,7 +148,7 @@ namespace IDEA.App
                     var selectedKontrola = db.Kontrola_Jakosci_Zamowienia.FirstOrDefault(k => k.ID_Kontrola_Jakosci_Zamowienia == id); // pobieramy obiekt Kontrola_Jakosci_Zamowienia o podanym ID
                     db.Kontrola_Jakosci_Zamowienia.Remove(selectedKontrola); // usuwamy obiekt Kontrola_Jakosci_Zamowienia z bazy danych
                     db.SaveChanges(); // zapisujemy zmiany w bazie danych
-                    refreshDgwKlienci();
+                    initDgwKlienci();
                     initDgwKlienci();
                     MessageBox.Show("Usunięto pomyślnie."); // wyświetlamy komunikat o sukcesie usuwania
                 }
